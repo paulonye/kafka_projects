6ed266eba885cc40bdbf8e913a5eced6c9c89f32
paulonye <nwosupaul141@gmail.com>
2023-06-07 11:45:38 +0100

    Modified datagen loadfile for postgres db

--- cdc_kafka_project/load_db.py
+++ cdc_kafka_project/load_db.py
@@ -4,27 +4,29 @@ import csv
 from time import ctime, sleep
 
 #reading in the file object
-file_object = open('acc_records.csv', "r")
-ext = csv.reader(file_object)
-
-#skipping row heaer
-header = next(ext)
+file_object = open('../assests/acc_records.csv', "r")
+ext = csv.DictReader(file_object)
 
 #make a connection to the database
 conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' port=5433 password='postgres'")
 curr = conn.cursor()
-print('here')
 
 #simulating real time data transfer to DB
 for row in ext:
+	#date, customer_id, payment_method, delivery_status, duration, orders, amount = row
 	curr.execute(
             "INSERT INTO public.transactions (date, customer_id, payment_method, delivery_status, duration, orders, amount) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-            (row[0], row[1], row[2], row[3], row[4], row[5], row[6]),)
+            #(date, customer_id, payment_method, delivery_status, duration, orders, amount),)
+			(row["date"], row["customer_id"], row["payment_method"], row["delivery_status"], row["duration"], row["orders"], row["amount"]),)
 	#print(df_row)
 	sleep(15)
 	conn.commit()
 	print(f'committed to db at: {ctime()}')
+
+#closing database connection
 curr.close()
+#closing the file object
+file_object.close()
 
 
 
